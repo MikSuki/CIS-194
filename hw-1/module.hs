d3114cb99af2dd1a40df708e7964344c25483172
MikSuki <34766025+MikSuki@users.noreply.github.com>
2024-06-30 01:30:56 +0800

    integrate exercise 1, 2 to module.hs

---
+++ hw-1/module.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module Module where
+
+-- exercise 1
+toDigits ::  Integer -> [Integer]
+toDigits n  
+    | n > 0 = toDigits (n `div` 10) ++ [n `mod` 10]
+    | otherwise = []
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n 
+    | n > 0 = [n `mod` 10] ++ toDigitsRev(n `div` 10)
+    | otherwise = []
+
+-- exercise 2
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther  (reverse -> (x : y : rs)) = doubleEveryOther (reverse rs) ++ [2 * y, x]
+doubleEveryOther [x] = [x]
+doubleEveryOther _ = []
+
+-- exercise 3
+-- exercise 4
+-- exercise 5
+-- exercise 6
